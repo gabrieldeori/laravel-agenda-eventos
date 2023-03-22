f1dced47ef9ca1660cd6b1cbf67a657bd0cc443b
gabrieldeori <deori.pessoal@gmail.com>
2023-03-22 17:06:49 -0300

    feat: ionicons added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -39,5 +39,7 @@
   <footer>Eventos &copy;2030</footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   <script src="/js/script.js"></script>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
