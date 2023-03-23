56a8c96afb39ccedc645b992eefe65fcd89b5215
gabrieldeori <deori.pessoal@gmail.com>
2023-03-23 17:01:44 -0300

    feat: adicionar banner ao evento

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -24,6 +24,16 @@ class EventController extends Controller
       $event->private = $request->private;
       $event->description = $request->description;
 
+      if ($request->hasFile('image') && $request->file('image')->isValid()) {
+        $requestImage = $request->image;
+        $extension = $requestImage->extension();
+        $imageName = md5($requestImage->getClientOriginalName() . strtoTime("now") . "." . $extension);
+        $requestImage->move(public_path('img/events'), $imageName);
+        $event->image = $imageName;
+      } else {
+        $event->image = "event_placeholder.jpg";
+      }
+
       $event->save();
 
       return redirect('/')->with('msg', 'Evento criado com sucesso!');
